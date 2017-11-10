ba9434287e3a3727387da5984e61f8c76a1a6179
Leonardo Alves <leoves.oliveira@gmail.com>
2017-11-10 14:40:55 -0200

    conexão com banco de dados

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,8 +1,20 @@
-<?php include 'cabecalho.php'; ?>
 <?php
-    $nome = $_GET["nome"];
-    $preco = $_GET["preco"];
-?>
+include 'cabecalho.php';
 
-    <p>Produto <?php echo $nome; ?>, de valor R$ <?php echo $preco; ?>, adicionado com sucesso!</p>
-<?php include 'rodape.php'; ?>
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+
+$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+if (mysqli_query($conexao, $query)) {
+?>
+<p class="text-success">Produto <?ph   p echo $nome; ?>, de valor R$ <?php echo $preco; ?>, adicionado com sucesso!</p>
+<?php
+} else {
+?>
+<p class="text-danger">Produto <?php echo $nome; ?> não foi adicionado!</p>
+<?php
+}
+include 'rodape.php';
+?>
