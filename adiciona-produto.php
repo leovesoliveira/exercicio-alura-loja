883f6a45ec45bca383c8830a0904d45d760d2fb7
Leonardo Alves <leoves.oliveira@gmail.com>
2017-11-13 13:32:25 -0200

    adicionado menu e função insereProduto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,19 +1,24 @@
 <?php
 include 'cabecalho.php';
 
+function insereProduto($conexao, $nome, $preco) {
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    return mysqli_query($conexao, $query);
+}
+
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
 
-$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
 $conexao = mysqli_connect('localhost', 'root', '', 'loja');
 
-if (mysqli_query($conexao, $query)) {
+if (insereProduto($conexao, $nome, $preco)) {
 ?>
 <p class="text-success">Produto <?ph   p echo $nome; ?>, de valor R$ <?php echo $preco; ?>, adicionado com sucesso!</p>
 <?php
 } else {
+    $msgError = mysqli_error($conexao);
 ?>
-<p class="text-danger">Produto <?php echo $nome; ?> não foi adicionado!</p>
+<p class="text-danger">Produto <?php echo $nome; ?> não foi adicionado: <?php echo $msgError; ?></p>
 <?php
 }
 include 'rodape.php';
--- cabecalho.php
+++ cabecalho.php
@@ -6,6 +6,20 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+    <div class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">Leoves'Store</a>
+            </div>
+
+            <div>
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="produto-formulario.php">Adicionar Produto</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="all">
 <!-- FINAL CABEÇALHO -->
